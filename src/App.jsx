a4e4f5d935e96db320df14fa8afabe2e1ae25543
Sonya <moonjuice1321@gmail.com>
2025-03-17 22:49:26 +0400

    - Update user state structure to support multiple entries  - Changed Experience, Education, Skills, and Languages from objects to arrays - Updated handleChange function - Implemented addField function to dynamically add new entries

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import CvFields from '../components/CvFields';
 import DataForm from '../components/DataForm';
 import PersonalDPage from '../components/PersonalDPage';
 import ExperiencePage from '../components/ExperiencePage';
-import Input from '../components/Input';
 import EducationPage from '../components/EducationPage';
 import SkillsPage from '../components/SkillsPage';
 import LanguagesPage from '../components/LanguagesPage';
@@ -25,7 +24,11 @@ function App() {
       City: 'Amsterdam',
       Country: 'Netherlands'
     },
-  Experience: {
+    Experience: [],
+    Education: [],
+    Skills: [],
+    Languages: []
+  /*Experience: {
     JobTitle: '',
     companyName : '',
     companyWebsite : '',
@@ -49,12 +52,27 @@ Proficiency: ''
   Languages: {
   Language: '',
   Proficiency: ''
-  }
+  }*/
 })
 const currentYear = new Date().getFullYear();//get current date/get exact year
 const years = Array.from({ length: 20}, (_, i) => currentYear - i);//new array create, mapping in reverse
 
-const handleChange = (section, field, value)  => {
+const handleChange = (section, index, field, value) => {
+  setUser((prevUser) => {
+    const updatedSection = [...prevUser[section]];//copy the array
+    updatedSection[index] = {...updatedSection[index], [field]: value};//ipdate field
+    return {...prevUser, [section] : updatedSection}
+  })
+}
+
+const addField = (section, newField) =>{
+  setUser((prevUser) => ({
+    ...prevUser,
+    [section]: [...prevUser[section], {...newField, id: Math.random() }]
+  })
+);
+}
+/*const handleChange = (section, field, value)  => {
   setUser((prevUser) => ({
       ...prevUser,
       [section]: {
@@ -63,15 +81,15 @@ const handleChange = (section, field, value)  => {
       
       }
   }));
- };
+ };*/
     const [ page, setPage] = useState('DataForm');
     const pages = {
       DataForm: <DataForm setPage={setPage} />,
       PersonalDPage: <PersonalDPage user ={user} setUser={setUser} setPage={setPage} />,
-      ExperiencePage: <ExperiencePage  user ={user} setUser ={setUser} setPage={setPage} handleChange={handleChange} years={years}/>,
-      EducationPage: <EducationPage user ={user} setUser ={setUser} setPage={setPage} handleChange={handleChange} years= {years}/>,
-      SkillsPage: <SkillsPage user ={user} setUser= {setUser} setPage={setPage} handleChange={handleChange} />,
-      LanguagesPage: <LanguagesPage user={user} setUser= {setUser} setPage={setPage} handleChange={handleChange} />
+      ExperiencePage: <ExperiencePage  user ={user} setUser ={setUser} setPage={setPage} handleChange={handleChange} years={years} addField={addField}/>,
+      EducationPage: <EducationPage user ={user} setUser ={setUser} setPage={setPage} handleChange={handleChange} years= {years} addField={addField}/>,
+      SkillsPage: <SkillsPage user ={user} setUser= {setUser} setPage={setPage} handleChange={handleChange} addField={addField}/>,
+      LanguagesPage: <LanguagesPage user={user} setUser= {setUser} setPage={setPage} handleChange={handleChange} addField={addField}/>
     }
   
   return(
